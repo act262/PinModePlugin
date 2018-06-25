1c886a31dd05d5a90fd284e45a83008dbf26ddd5
act262 <act262@gmail.com>
2018-06-25 20:20:11 +0800

    feat(demo): Update multiple buildType and productFlavor use case

---
+++ demo/app/m_app2/src/flavor2/java/com/jfz/app/app2/Flavor2App1Activity.kt
@@ -0,0 +1,16 @@
+package com.jfz.app.app1
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by act.zhang on 2018/6/20.
+ *
+ * @author zhangchaoxian@jinfuzi.com
+ */
+class Flavor2App1Activity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        print("hello")
+    }
+}
